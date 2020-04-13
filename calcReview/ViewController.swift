22b2c7d1aebc77c3e6582beb6907a9a3ab0d86b2
Iram Fattah <iramfattah94@gmail.com>
2020-04-13 15:00:26 -0400

    finished calc brain model

---
+++ calcReview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calcReview
+//
+//  Created by C4Q on 4/13/20.
+//  Copyright © 2020 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
